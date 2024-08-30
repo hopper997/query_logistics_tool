b5b8a7ec3a79240270c03de714700b6470b97bd5
王志刚 <1376392247@qq.com>
2024-08-31 00:30:35 +0800

    提交data文件夹

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from scripts.track_and_parse import register_logistic_no, query_logistic_detail, write_logistic_detail_to_csv
+from scripts.track_and_parse import write_logistic_detail_to_csv
 
 
 def main():
